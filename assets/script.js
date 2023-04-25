ff17eb59e173dc16a6e7977cc45ea1dd8a7a5a09
Aubrey McKinney <99014955+Shadowasders@users.noreply.github.com>
2023-04-25 16:34:42 -0400

    declared variables

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,13 @@
 /*Things to add!
-Intregrate Dayjs
 Intregrate weather API
 add functionality to search button (eventListener on click)
 use local storage to save past searches and add them below search bar (see previous classes for this? Unsure)
 popluate empty divs w/information on search button press (use for loop for buttons, how to add to the next day in a for loop?) 
 find way to use the lil weather emojis for each type of weather, and figure out how to add them into the JS (insert, modify, append? where to find them to link w/webpage?)
 once js is functional style site*/
+var time = dayjs();
+var citySearchEl = document.querySelector(".subbutton");
+var searchHistoryEl = document.querySelector(".pastsearch");
+var cityFeatured = document.querySelector(".mainname");
+var cityDetails = document.querySelector(".maindesc");
+var futureDetails = document.querySelector(".futuretemps"); 
