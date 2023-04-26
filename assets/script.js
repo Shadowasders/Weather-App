7b571e3b05f8b14aa391114e4a49254f026b2c30
Aubrey McKinney <99014955+Shadowasders@users.noreply.github.com>
2023-04-25 20:14:26 -0400

    added api calls for translating city name to lat/lon

--- assets/script.js
+++ assets/script.js
@@ -5,9 +5,55 @@ use local storage to save past searches and add them below search bar (see previ
 popluate empty divs w/information on search button press (use for loop for buttons, how to add to the next day in a for loop?) 
 find way to use the lil weather emojis for each type of weather, and figure out how to add them into the JS (insert, modify, append? where to find them to link w/webpage?)
 once js is functional style site*/
-var time = dayjs();
+//use template literals ${} in the fetch URL
+var date = dayjs();
+var cityNameEl = document.querySelector(".cityname");
 var citySearchEl = document.querySelector(".subbutton");
-var searchHistoryEl = document.querySelector(".pastsearch");
+var searchHistoryEl = document.querySelector(".searches");
 var cityFeatured = document.querySelector(".mainname");
 var cityDetails = document.querySelector(".maindesc");
 var futureDetails = document.querySelector(".futuretemps"); 
+var apiKey = "c3644d4bd3928fab3c281a08091b70b0";
+var cityLat = ""
+var cityLon = ""
+
+function findCoord() {
+var cityParse = citySearchEl.value;
+fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityParse}&limit=1&appid=${apiKey}`)
+    .then (function (response) {
+        return response.json();
+    })
+    .then (function (data){
+        cityLat = data[0].lat;
+        console.log(cityLat);
+        cityLon = data[0].lon;
+        console.log(cityLon); 
+    })
+    .then(weatherReport);
+    // console.log(cityLat);
+    // console.log(cityLon);
+
+
+function weatherReport(){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${cityLat}&lon=${cityLon}&appid=${apiKey}`)
+    .then (function (response) {
+        return response.json
+    })
+    .then (function (data) {
+        console.log(data);
+    })
+};
+}
+
+findCoord();
+// citySearchEl.addEventListener("click", )
+
+//TODO: past searches help (from class)
+//function rendersearchHistory(){ 
+//var searchHistory = JSON.parse(localStorage.getItem("cityHistory(set past searches to localStorage w/this name)")) || [];
+//for (i = 0; i < cityHistory.length; i++){
+//var pastCity = document.createElement("button");
+// pastCity.setAttribute("class", "TBD");
+//cityButton.textContent(searchHistory[i]);
+//searchHistoryEL.appendChild(pastCity);
+//}};
